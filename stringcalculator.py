5da4a032d751ae5910aee367d054a232af13f44e
Azzedev <azzedine.dev@outlook.Fr>
2024-03-13 08:49:47 +0100

    Basic test

---
+++ stringcalculator.py
@@ -0,0 +1,4 @@
+def stringcalculator(a,b):
+    a = int(a)
+    b = int(b)
+    return a+b
---
+++ test_stringcalculator.py
@@ -0,0 +1,18 @@
+import pytest
+import random
+from stringcalculator import stringcalculator
+
+@pytest.mark.parametrize("a, b, expected", [
+    (1, 0, 1),
+    (1, 1, 2),
+])
+def test_stringcalculator(a, b, expected):
+    assert stringcalculator(a, b) == expected
+
+
+def test_stringcalculator_random():
+    for _ in range(10):
+        a = random.randint(1, 1000)
+        b = random.randint(1, 1000)
+        expected = a + b
+        assert stringcalculator(a,b) == expected
